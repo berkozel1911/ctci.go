a671fdb3fcdf3d1417ca656abd016d9959609fd8
Berk Özel <berkozel@protonmail.com>
2024-03-20 23:54:56 +0300

    Two Sum

--- 01-leetcode/01-easy/1.go
+++ 01-leetcode/01-easy/1.go
@@ -1,19 +1,9 @@
-/*
-
-1. Two Sum
-
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-*/
-
+// 1. Two Sum
 package main
 
 import "fmt"
 
-func twoSum(nums [3]int, target int) [2]int {
+func twoSum(nums [3]int, target int) []int {
    res := []int{}
    for i:=0; i < len(nums)-1; i++ {
       for j:=i+1; j < len(nums); j++ {
